fa15db27493a5e60af94f920f85b1afabadb0c19
Tonyplaza <plazatony3@gmail.com>
2019-11-12 22:46:54 -0500

    Tarea Area Triangulo

---
+++ AppFuncionesyproc/TareaAreaTrian.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace AppFuncionesyproc
+{
+    partial class TareaAreaTrian
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 75);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "c:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Area:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(288, 49);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(288, 79);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 5;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(288, 109);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(116, 49);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 7;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(116, 75);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 8;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(116, 101);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 9;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(116, 128);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 10;
+            // 
+            // TareaAreaTrian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 225);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TareaAreaTrian";
+            this.Text = "TareaAreaTrian";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtArea;
+    }
+}
---
+++ AppFuncionesyproc/TareaAreaTrian.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppFuncionesyproc
+{
+    public partial class TareaAreaTrian : Form
+    {
+        public TareaAreaTrian()
+        {
+            InitializeComponent();
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private double Area(double a, double b, double c)
+        {
+            //declarar variables
+            double x1 = 0, p = 0, d = 0;
+
+            //calcular el discriminante
+            p = (a+b+c)/2;
+            d = p * (p - a) * (p - b) * (p - c);
+            if (d < 0)
+            {
+                MessageBox.Show("Error, la ecuacion tiene soluciones imaginarias");
+                return 0;
+            }
+            x1 = Math.Sqrt(d);
+            return x1;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //llamar a la funcion calcular x1
+            double a, b, c;
+            if (this.txtA.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor ingrese el valor de A.");
+                this.txtA.Focus();//ubica el cursor en el cuadro de texto
+                return;
+            }
+            if (this.txtB.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor ingrese el valor de B.");
+                this.txtB.Focus();//ubica el cursor en el cuadro de texto
+                return;
+            }
+            if (this.txtC.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor ingrese el valor de C.");
+                this.txtC.Focus();//ubica el cursor en el cuadro de texto
+                return;
+            }
+            a = Double.Parse(this.txtA.Text);
+            b = Double.Parse(this.txtB.Text);
+            c = Double.Parse(this.txtC.Text);
+            double x1 = Area(a, b, c);
+            //asigno el resultado de x1 en el tectbox txtX1
+            this.txtArea.Text = x1.ToString();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            this.txtA.Text = String.Empty;
+            this.txtB.Text = String.Empty;
+            this.txtC.Text = String.Empty;
+            this.txtArea.Text = String.Empty;
+            this.txtA.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

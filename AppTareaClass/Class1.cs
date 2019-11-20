ab3965e1eb8ce58106c229fd21169e6d14386073
Tonyplaza <plazatony3@gmail.com>
2019-11-19 20:44:00 -0500

    Class  para formula general x1 y x2

---
+++ AppTareaClass/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTareaClass
+{
+    public static class Formula
+    {
+        public static double x1(double a, double b, double c)
+        {
+            double x1 = 0, d = 0;
+            d = (b * b) - (4 * a * c);
+            x1 = (-b - Math.Sqrt(d)) / (2 * a);
+            return x1;
+        }
+        public static double x2(double a, double b, double c)
+        {
+            double x2 = 0, d = 0;
+            d = (b * b) - (4 * a * c);
+            x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            return x2;
+        } 
+    }
+}

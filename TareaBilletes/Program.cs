825e506386bcf0a6158a5c5228568f197c960ddf
Tonyplaza <plazatony3@gmail.com>
2019-11-20 12:47:29 -0500

    Tarea Cambio equivalencia de dolares billetes y monedas

--- TareaBilletes/Program.cs
+++ TareaBilletes/Program.cs
@@ -10,7 +10,19 @@ namespace TareaBilletes
     {
         static void Main(string[] args)
         {
-
+            cambio(27.50);
+            Console.ReadKey();
+        }
+        static void cambio(double dolares)
+        {
+            int billete10 = (int)dolares / 10;
+            Console.WriteLine("{0} billetes de 10 dolares",billete10);
+            int billete5 = ((int)dolares % 10) / 5;
+            Console.WriteLine("{0} billetes de 5 dolares", billete5);
+            int monedadolar = ((int)dolares % 10) % 5;
+            Console.WriteLine("{0} monedas de dolar ", monedadolar);
+            double monedacentavos = ((dolares % 10) % 5)%1;
+            Console.WriteLine("{0} centavos de dolar ", monedacentavos);
         }
     }
 }
